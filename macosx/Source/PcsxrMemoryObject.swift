cae91bd0aa18501bc19302921b89df548a329dc7
C.W. Betts <computers57@hotmail.com>
2015-07-19 11:57:23 -0600

    Update imagesFromMcd to be more like its Obj-C counterpart.

--- macosx/Source/PcsxrMemoryObject.swift
+++ macosx/Source/PcsxrMemoryObject.swift
@@ -18,27 +18,32 @@ import SwiftAdditions
 };
 
 private func imagesFromMcd(theBlock: UnsafePointer<McdBlock>) -> [NSImage] {
+	struct PSXRGBColor {
+		var r: UInt8
+		var g: UInt8
+		var b: UInt8
+	};
+
 	var toRet = [NSImage]()
 	let unwrapped = theBlock.memory
 	let iconArray: [Int16] = getArrayFromMirror(reflect(unwrapped.Icon))
 	for i in 0..<unwrapped.IconCount {
-		autoreleasepool() {
-			if let imageRep = NSBitmapImageRep(bitmapDataPlanes: nil, pixelsWide: 16, pixelsHigh: 16, bitsPerSample: 8, samplesPerPixel: 3, hasAlpha: false, isPlanar: false, colorSpaceName: NSCalibratedRGBColorSpace, bytesPerRow: 0, bitsPerPixel: 0) {
+			if let imageRep = NSBitmapImageRep(bitmapDataPlanes: nil, pixelsWide: 16, pixelsHigh: 16, bitsPerSample: 8, samplesPerPixel: 3, hasAlpha: false, isPlanar: false, colorSpaceName: NSCalibratedRGBColorSpace, bytesPerRow: 16 * 3, bitsPerPixel: 24) {
+				let cocoaImageData = UnsafeMutablePointer<PSXRGBColor>(imageRep.bitmapData)
 				for v in 0..<256 {
-					let x = v % 16
-					let y = v / 16
+					//let x = v % 16
+					//let y = v / 16
 					let c = iconArray[Int(i * 256) + v]
 					let r: Int32 = Int32(c & 0x001F) << 3
 					let g: Int32 = (Int32(c & 0x03E0) >> 5) << 3
 					let b: Int32 = (Int32(c & 0x7C00) >> 10) << 3
-					imageRep.setColor(NSColor(calibratedRed: CGFloat(r) / 255.0, green: CGFloat(g) / 255.0, blue: CGFloat(b) / 255.0, alpha: 1), atX: x, y: y)
+					cocoaImageData[v] = PSXRGBColor(r: UInt8(r), g: UInt8(g), b: UInt8(b))
 				}
 				let memImage = NSImage()
 				memImage.addRepresentation(imageRep)
 				memImage.size = NSSize(width: 32, height: 32)
 				toRet.append(memImage)
 			}
-		}
 	}
 	return toRet
 }
